65704e34dd9b0544a3c24fa109579edd81d6b4d4
Jianchen Liu <56807062+kenchan-liu@users.noreply.github.com>
2023-04-27 21:44:18 +0800

    Update roulette.sol

--- roulette.sol
+++ roulette.sol
@@ -20,17 +20,52 @@ contract Roulette {
         uint bettingLimitMax;
     }
     Casino public casino;
+    event ReturnValue(bytes32 myData);
 
     // Init Constructor
     constructor() public{
         sWelcome = "\n-----------------------------\n     Welcome to Roulette \n Got coins? Then come on in! \n-----------------------------\n";
         privSeed = 1;
+
         casino.addr = msg.sender;
         casino.balance = 1000*10;
         casino.bettingLimitMin = 1;
         casino.bettingLimitMax = 10;
     }
-    function getseed() public returns(uint){
+    uint[]mt;
+    uint index;
+    function initialize(uint seed) public {
+        mt = new uint[](624);
+        mt[0] = seed;
+        for (uint i = 1; i < 624; i++) {
+            mt[i] = uint(1812433253) * (mt[i-1] ^ (mt[i-1] >> 30)) + i;
+        }
+        index = 0;
+    }
+
+    function twist() private {
+        uint y;
+        for (uint i = 0; i < 624; i++) {
+            y = (mt[i] & uint(0x80000000)) + 
+                (mt[(i+1) % 624] & uint(0x7fffffff));
+            mt[i] = mt[(i+397) % 624] ^ (y >> 1);
+            if (y % 2 != 0) {
+                mt[i] = mt[i] ^ uint(0x9908b0df);
+            }
+        }
+    }
+    function extractNumber() public returns (uint) {
+        twist();
+        uint y = mt[0];
+        y = y ^ (y >> 11);
+        y = y ^ ((y << 7) & 2636928640);
+        y = y ^ ((y << 15) & 4022730752);
+        y = y ^ (y >> 18);
+        index = (index + 1) % 624;
+        return y;
+    }
+
+    function getseed() public view returns(uint){
         return privSeed;
     }
     function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
@@ -85,36 +120,40 @@ contract Roulette {
     // Bet on Number
     function betOnNumber(uint number) payable public returns (string) {
         // Input Handling
-        address addr = msg.sender;
-        uint betSize = msg.value;
-        if (betSize < casino.bettingLimitMin || betSize > casino.bettingLimitMax) {
-            // Return Funds
-            if (betSize >= 1)
-                addr.transfer(betSize);
-            return uint2str(betSize);
-        }
-        if (betSize * 36 > casino.balance) {
-            // Return Funds
-            addr.transfer(betSize);
-            return "Casino has insufficient funds for this bet amount";
-        }
+        // address addr = msg.sender;
+        // uint betSize = msg.value;
+        // if (betSize < casino.bettingLimitMin || betSize > casino.bettingLimitMax) {
+        //     // Return Funds
+        //     if (betSize >= 1)
+        //         addr.transfer(betSize);
+        //     return uint2str(betSize);
+        // }
+        // if (betSize * 36 > casino.balance) {
+        //     // Return Funds
+        //     addr.transfer(betSize);
+        //     return "Casino has insufficient funds for this bet amount";
+        // }
         if (number < 0 || number > 36) {
             // Return Funds
-            addr.transfer(betSize);
+            //addr.transfer(betSize);
+            emit ReturnValue(bytes32("number"));
+
             return "Please choose a number between 0 and 36";
         }
         // Roll the wheel
         privSeed += 1;
         uint rand = generateRand();
         if (number == rand) {
-            // Winner winner chicken dinner!
-            uint winAmount = betSize * 36;
-            casino.balance -= (winAmount - betSize);
-            addr.transfer(winAmount);
-            return "Winner winner chicken dinner!";
+            // // Winner winner chicken dinner!
+            // uint winAmount = betSize * 36;
+            // casino.balance -= (winAmount - betSize);
+            // addr.transfer(winAmount);
+            emit ReturnValue(bytes32("winner"));
+            return "winner";
         }
         else {
-            casino.balance += betSize;
+            //casino.balance += betSize;
+            emit ReturnValue(bytes32("rand"));
             return uint2str(rand);
         }
     }
@@ -160,12 +199,11 @@ contract Roulette {
     // Returns a pseudo Random number.
     function generateRand() private returns (uint) { 
         // Seeds
-        privSeed = (privSeed*3 + 1) / 2;
-        privSeed = privSeed % 10**9;
+        privSeed = extractNumber();
         uint number = block.number; // ~ 10**5 ; 60000
         uint diff = block.difficulty; // ~ 2 Tera = 2*10**12; 1731430114620
         uint time = block.timestamp; // ~ 2 Giga = 2*10**9; 1439147273
-        uint gas = block.gasLimit; // ~ 3 Mega = 3*10**6
+        uint gas = block.gaslimit; // ~ 3 Mega = 3*10**6
         // Rand Number in Percent
         uint total = privSeed + number + diff + time + gas;
         uint rand = total % 37;
